d82dcb916e77c4ff351c17883a7f246f1f8b9303
Sang Min Park <9714park@gmail.com>
2020-03-27 14:47:56 -0400

    Updated routes for TermCreate and TermDetail component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TermsListComponent } from './terms/terms-list/terms-list.component';
+import { TermDetailComponent } from './terms/term-detail/term-detail.component';
+import { TermsCreateComponent } from './terms/terms-create/terms-create.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'terms', component: TermsListComponent },
+  { path: 'terms/create', component: TermsCreateComponent },
+  { path: 'terms/detail/:id', component: TermDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
