574179ac773bb1a02396053280539e4c1af890f5
Sang Min Park <9714park@gmail.com>
2020-03-27 14:49:39 -0400

    Added FormsModule and AppRoutingModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { HomeComponent } from './home/home.component';
+import { TermsListComponent } from './terms/terms-list/terms-list.component';
+import { TermDetailComponent } from './terms/term-detail/term-detail.component';
+import { TermsCreateComponent } from './terms/terms-create/terms-create.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavComponent,
+    HomeComponent,
+    TermsListComponent,
+    TermDetailComponent,
+    TermsCreateComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
