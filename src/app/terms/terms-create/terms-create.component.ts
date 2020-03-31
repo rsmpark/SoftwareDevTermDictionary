57447126974e1c4ba12528c28ffac43e3c60cd18
Sang Min Park <9714park@gmail.com>
2020-03-31 11:59:32 -0400

    Complete term-create-component_v1.0.

--- src/app/terms/terms-create/terms-create.component.ts
+++ src/app/terms/terms-create/terms-create.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { EnglishTerm, EnglishTermCreate } from 'src/app/shared/model/term.model';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  EnglishTermApi,
+  EnglishTermRequest,
+  DefinitionRequest
+} from 'src/app/shared/model/term.model';
 import { DataManagerService } from 'src/app/shared/data-manager.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-terms-create',
@@ -8,22 +14,77 @@ import { DataManagerService } from 'src/app/shared/data-manager.service';
   styleUrls: ['./terms-create.component.css']
 })
 export class TermsCreateComponent implements OnInit {
-  newTerm: EnglishTermCreate;
-  termResult: EnglishTerm;
-  currentTerms: EnglishTerm[];
+  newTerm: DataForm;
+  termResult: EnglishTermApi;
 
-  constructor(private dataManager: DataManagerService) {
-    this.newTerm = new EnglishTerm();
-    this.termResult = new EnglishTerm();
+  @ViewChild('f') public termCreateForm: NgForm;
+
+  constructor(private dataManager: DataManagerService, private router: Router) {
+    this.newTerm = new DataForm();
+    this.termResult = new EnglishTermApi();
   }
 
-  ngOnInit(): void {
-    this.dataManager.getAllEnglishTerms().subscribe(terms => {
-      this.currentTerms = terms;
-    });
-  }
+  ngOnInit(): void {}
 
   onTermSave() {
-    console.log(this.newTerm);
+    if (this.termCreateForm.valid) {
+      this.sendCreateRequest();
+      this.router.navigate([`terms/detail/${this.termResult._id}`]);
+    }
   }
+
+  private sendCreateRequest() {
+    const createRequestData = new EnglishTermRequest();
+
+    // TODO: What if wordExpanded needs modification?
+    const { authorName, wordEnglish, definition } = this.newTerm;
+
+    // Modify form data
+    this.newTerm.authorName = authorName.trim();
+    this.newTerm.wordEnglish = wordEnglish.trim();
+
+    // Create definitionData
+    const definitionData = new DefinitionRequest(this.newTerm.authorName, definition);
+    createRequestData.definitions = [definitionData];
+
+    // Create data package to send for POST request
+    const dataPackage = {
+      ...createRequestData,
+      wordNonEnglish: '',
+      wordExpanded: '',
+      languageCode: 'en',
+      image: '',
+      imageType: '',
+      audio: '',
+      audioType: '',
+      linkAuthoritative: '',
+      linkWikipedia: '',
+      linkYouTube: '',
+      authorName: '',
+      fieldOfStudy: '',
+      helpYes: 0,
+      helpNo: 0,
+      ...this.newTerm
+    };
+
+    // Remove unnecessary properties
+    delete dataPackage.definition;
+
+    // this.dataManager
+    //   .addEnglishTerm(dataPackage)
+    //   .subscribe(result => (this.termResult = result));
+
+    // TODO: Remove log
+    console.log(this.termResult);
+  }
+}
+
+class DataForm {
+  authorName: string;
+  wordEnglish: string;
+  wordExpanded?: string;
+  definition: string;
+  linkAuthoritative?: string;
+  linkWikipedia?: string;
+  linkYoutube?: string;
 }
