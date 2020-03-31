dcda972dd893f16bf146ba5aea931247a28bc09f
Sang Min Park <9714park@gmail.com>
2020-03-31 12:02:42 -0400

    Add template to term-detail-component.

--- src/app/terms/term-detail/term-detail.component.ts
+++ src/app/terms/term-detail/term-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataManagerService } from 'src/app/shared/data-manager.service';
+import { EnglishTermApi } from 'src/app/shared/model/term.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-term-detail',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./term-detail.component.css']
 })
 export class TermDetailComponent implements OnInit {
+  term: EnglishTermApi;
 
-  constructor() { }
+  constructor(private dataManager: DataManagerService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+    this.dataManager.getEnglishTermById(id).subscribe(termResult => {
+      this.term = termResult;
+      console.log(this.term);
+    });
   }
-
 }
