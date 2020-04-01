3ece2a0efa470429a4f49dacc271944781f7c0cd
Sang Min Park <9714park@gmail.com>
2020-04-01 12:37:02 -0400

    Add TermDetailComponent to routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { TermsListComponent } from './terms/terms-list/terms-list.component';
 import { TermDetailComponent } from './terms/term-detail/term-detail.component';
 import { TermsCreateComponent } from './terms/terms-create/terms-create.component';
+import { DefinitionEditComponent } from './definitions/definition-edit/definition-edit.component';
 
 const routes: Routes = [
   { path: 'terms', component: TermsListComponent },
   { path: 'terms/create', component: TermsCreateComponent },
-  { path: 'terms/detail/:id', component: TermDetailComponent }
+  { path: 'terms/detail/:id', component: TermDetailComponent },
+  { path: 'terms/detail/:id/edit-definition', component: DefinitionEditComponent }
 ];
 
 @NgModule({
