1c42cefe0e1c074a25f35eaccc8445d2c1721593
Sang Min Park <9714park@gmail.com>
2020-04-05 01:47:22 -0400

    Add AddTranslationComponent.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,18 +6,20 @@ import { TermsCreateComponent } from './terms/terms-create/terms-create.componen
 import { DefinitionEditComponent } from './definitions/definition-edit/definition-edit.component';
 import { TermOtherDetailComponent } from './terms/term-detail/term-other-detail.component';
 import { DefinitionOtherEditComponent } from './definitions/definition-edit/definition-other-edit.component';
+import { AddTranslationComponent } from './translation/add-translation/add-translation.component';
 
 const routes: Routes = [
   { path: 'termsEnglish', component: TermsListComponent },
   { path: 'termsEnglish/create', component: TermsCreateComponent },
   { path: 'termsEnglish/detail/:id', component: TermEnglishDetailComponent },
   { path: 'termsEnglish/detail/:id/edit-definition', component: DefinitionEditComponent },
+  { path: 'termsOther/create/:id', component: AddTranslationComponent },
   { path: 'termsOther/detail/:id', component: TermOtherDetailComponent },
-  { path: 'termsOther/detail/:id/edit-definition', component: DefinitionOtherEditComponent }
+  { path: 'termsOther/detail/:id/edit-definition', component: DefinitionOtherEditComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
