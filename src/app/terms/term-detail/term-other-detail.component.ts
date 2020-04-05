b84366feff79ba2a3849b1a278d4d5182e8a5a1e
Sang Min Park <9714park@gmail.com>
2020-04-05 01:46:31 -0400

    Add language the term is translated in the header.

--- src/app/terms/term-detail/term-other-detail.component.ts
+++ src/app/terms/term-detail/term-other-detail.component.ts
@@ -1,27 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { NonEnglishTermApi } from 'src/app/shared/model/term.model';
+import { NonEnglishTermApi, ISOLanguageCodeAPI } from 'src/app/shared/model/term.model';
 import { DataManagerService } from 'src/app/shared/data-manager.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-term-other-detail',
   templateUrl: './term-other-detail.component.html',
-  styleUrls: ['./term-other-detail.component.css']
+  styleUrls: ['./term-other-detail.component.css'],
 })
 export class TermOtherDetailComponent implements OnInit {
   term: NonEnglishTermApi;
+  languageCodes: ISOLanguageCodeAPI[];
+  languageName: string;
 
-  constructor(
-    private dataManager: DataManagerService,
-    private route: ActivatedRoute,
-    private router: Router
-  ) {}
+  constructor(private dataManager: DataManagerService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     // Get english term by ID
     const id = this.route.snapshot.params['id'];
-    this.dataManager.getNonEnglishTermById(id).subscribe(termResult => {
+
+    this.dataManager.getNonEnglishTermById(id).subscribe((termResult) => {
       this.term = termResult;
+
+      this.dataManager.getAllLanguageCodes().subscribe((results) => {
+        this.languageCodes = results;
+        let termsLanguage = this.languageCodes.find(
+          (langCode) => langCode.code === this.term.languageCode
+        );
+
+        this.languageName = termsLanguage.name;
+      });
     });
   }
 
@@ -30,16 +38,16 @@ export class TermOtherDetailComponent implements OnInit {
 
     this.dataManager
       .incrementLikesOther(selectedDefinition._id, { _id: selectedDefinition._id })
-      .subscribe(termResult => (this.term = termResult));
+      .subscribe((termResult) => (this.term = termResult));
   }
   onHelpYesClick() {
     this.dataManager
       .incrementHelpYesOther(this.term._id, { _id: this.term._id })
-      .subscribe(termResult => (this.term = termResult));
+      .subscribe((termResult) => (this.term = termResult));
   }
   onHelpNoClick() {
     this.dataManager
       .incrementHelpNoOther(this.term._id, { _id: this.term._id })
-      .subscribe(termResult => (this.term = termResult));
+      .subscribe((termResult) => (this.term = termResult));
   }
 }
