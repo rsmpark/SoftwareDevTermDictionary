1928e2e812748d7158b4a8c3a350a85557b90918
Sang Min Park <9714park@gmail.com>
2020-04-05 01:49:46 -0400

    Fix header.

--- src/app/terms/term-detail/term-detail.component.ts
+++ src/app/terms/term-detail/term-detail.component.ts
@@ -3,14 +3,14 @@ import { DataManagerService } from 'src/app/shared/data-manager.service';
 import {
   EnglishTermApi,
   NonEnglishTermApi,
-  ISOLanguageCodeAPI
+  ISOLanguageCodeAPI,
 } from 'src/app/shared/model/term.model';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-term-english-detail',
   templateUrl: './term-detail.component.html',
-  styleUrls: ['./term-detail.component.css']
+  styleUrls: ['./term-detail.component.css'],
 })
 export class TermEnglishDetailComponent implements OnInit {
   term: EnglishTermApi;
@@ -27,21 +27,21 @@ export class TermEnglishDetailComponent implements OnInit {
   ngOnInit(): void {
     // Get english term by ID
     const id = this.route.snapshot.params['id'];
-    this.dataManager.getEnglishTermById(id).subscribe(termResult => {
+    this.dataManager.getEnglishTermById(id).subscribe((termResult) => {
       this.term = termResult;
     });
 
     // TODO: Fix observable chaining
     // Get ISO language codes
-    this.dataManager.getAllLanguageCodes().subscribe(results => {
+    this.dataManager.getAllLanguageCodes().subscribe((results) => {
       this.languageCode = results;
 
       // Get translated terms
-      this.dataManager.getAllNonEnglishTerms().subscribe(termResults => {
+      this.dataManager.getAllNonEnglishTerms().subscribe((termResults) => {
         const tempTranslateTerms = [];
 
         // Map to find corresponding translation
-        termResults.map(termResult => {
+        termResults.map((termResult) => {
           if (termResult.termEnglishId === this.term._id) {
             tempTranslateTerms.push(termResult);
             this.languageNames.push(this.getLanguageNames(termResult.languageCode));
@@ -58,27 +58,29 @@ export class TermEnglishDetailComponent implements OnInit {
 
     this.dataManager
       .incrementLikes(selectedDefinition._id, { _id: selectedDefinition._id })
-      .subscribe(termResult => (this.term = termResult));
+      .subscribe((termResult) => (this.term = termResult));
   }
   onHelpYesClick() {
     this.dataManager
       .incrementHelpYes(this.term._id, { _id: this.term._id })
-      .subscribe(termResult => (this.term = termResult));
+      .subscribe((termResult) => (this.term = termResult));
   }
   onHelpNoClick() {
     this.dataManager
       .incrementHelpNo(this.term._id, { _id: this.term._id })
-      .subscribe(termResult => (this.term = termResult));
+      .subscribe((termResult) => (this.term = termResult));
   }
 
-  onAddTranslation() {}
+  onAddTranslation() {
+    this.router.navigate([`termsOther/create/${this.term._id}`]);
+  }
 
   onGetTranslationDetail(index: number) {
     this.router.navigate([`termsOther/detail/${this.translatedTerms[index]._id}`]);
   }
 
   private getLanguageNames(languageCode: string) {
-    const languageCodeFound = this.languageCode.find(result => result.code === languageCode);
+    const languageCodeFound = this.languageCode.find((result) => result.code === languageCode);
     return languageCodeFound.name;
   }
 }
